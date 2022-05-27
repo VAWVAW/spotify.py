c381c287dbe3573ea0e3ceb2905e2ed72279049f
vawvaw <valentin@wiedekind1.de>
2022-05-27 11:21:05 +0200

    Implement User

--- spotify/user.py
+++ spotify/user.py
@@ -3,8 +3,27 @@ from .connection import Connection
 
 class User:
     def __init__(self, id: str, connection: Connection, cache_dir: str = None, display_name: str = None):
-        #TODO implement User
-        pass
+        self._id = id
+        self._uri = "spotify:user:" + id
+        self._connection = connection
+        self._cache_dir = cache_dir
+        self._display_name = display_name
 
     def __dict__(self):
-        return {"id": "asdf", "display_name": "asdf"}
+        return {
+            "id": self._id,
+            "display_name": self._display_name,
+            "uri": self._uri
+            }
+
+    @property
+    def id(self) -> str:
+        return self._id
+
+    @property
+    def uri(self) -> str:
+        return self._uri
+
+    @property
+    def display_name(self) -> str:
+        return self._display_name
